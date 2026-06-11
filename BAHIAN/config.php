9e066911d1425b5dc7c0118fba02d8e228d13563
jesserebbahian <jesserebbahian@gmail.com>
2026-06-11 09:35:55 +0800

    Add files via upload

---
+++ BAHIAN/config.php
@@ -0,0 +1,15 @@
+<?php
+// config.php
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db   = 'lab1';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "";
+?>
